61321ebc72668b744f14375d684f583808542544
isabpp <96348348+isabpp@users.noreply.github.com>
2025-06-13 17:07:56 +0300

    updated renderHolidays and the UI

--- script.js
+++ script.js
@@ -28,10 +28,24 @@ function renderHolidays(data) {
 
     for (let i = 0; i < data.length; i++) {
         const holidayContainer = document.createElement('div');
-        holidayContainer.className = 'holiday-container';
+        holidayContainer.className = 'holiday_container';
+        const innerContainer = document.createElement('div');
+        innerContainer.className = 'inner_container';
+        const capsule1 = document.createElement('div');
+        capsule1.className = 'capsule1';
+        const capsule2 = document.createElement('div');
+        capsule2.className = 'capsule2';
+        
+        const date = new Date(data[i].date);
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-        holidayContainer.innerHTML = '<p>' + data[i].localName + '</p>';
+        capsule1.innerHTML = '<b>' + days[date.getDay()] + '</b> <br> <p>' + months[date.getMonth()].slice(0, 3) + ' ' + date.getDate() + ', ' + date.getFullYear() + '</p>';
+        capsule2.innerHTML = '<b>' + data[i].localName + '</b> <br> <p>' + data[i].name + '</p>';
 
+        innerContainer.appendChild(capsule1);
+        innerContainer.appendChild(capsule2);
+        holidayContainer.appendChild(innerContainer);
         contentContainer.appendChild(holidayContainer);
         console.log(data[i].localName);
     }
